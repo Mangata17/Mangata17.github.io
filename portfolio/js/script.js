b58d4222735a9817f9f40ffd7e64e2825b9866f7
Gopika Dharmarajan <gopikadhrma1997@gmail.com>
2020-07-13 20:09:09 +0530

    updatedfile

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,1 +1,9 @@
 // Custom js
+
+$(".sidenav-inner .menu-item").click(function (e) {
+    e.preventDefault();
+    var id = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 1000);
+});
